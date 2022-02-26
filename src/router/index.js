9f810a91e73cbf6653b4aea00fe2369c0e093e85
Sagar Yadav <sagaryadav17@outlook.com>
2022-02-27 00:57:22 +0530

    PWA update

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import YCombinator from "../views/YCombinator.vue";
 
 const routes = [
   {
@@ -10,7 +11,7 @@ const routes = [
   {
     path: "/y-combinator",
     name: "YCombinator",
-    component: () => import("../views/YCombinator.vue"),
+    component: YCombinator,
   },
 ];
 
--- vue.config.js
+++ vue.config.js
@@ -9,20 +9,8 @@ module.exports = {
     workboxOptions: {
       skipWaiting: true,
     },
-    manifestOptions: {
-      icons: [
-        {
-          src: "/android-chrome-192x192.png",
-          src: "public/img/icons/android-chrome-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "public/img/icons/android-chrome-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
+    iconPaths: {
+      msTileImage: "img/icons/mstile-150x150.png",
     },
   },
 };
