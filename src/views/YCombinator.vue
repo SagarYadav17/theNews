941d159ff5a60cea64ea6b38be9a9931c236bda1
Sagar Yadav <sagaryadav17@outlook.com>
2022-02-27 00:35:22 +0530

    YCombinator API integrated

--- src/views/YCombinator.vue
+++ src/views/YCombinator.vue
@@ -2,12 +2,12 @@
   <Header />
 
   <main v-if="!loading">
-    <div class="flex flex-col items-start justify-center max-w-2xl mx-auto mt-5 mb-16">
+    <div class="flex flex-col items-start justify-center px-8 max-w-4xl mx-auto mt-5 mb-16">
       <a class="w-full">
-        <div v-for="post in stories" v-bind:key="post" class="w-full mb-8">
+        <div v-for="story in stories" v-bind:key="story" class="w-full mb-8">
           <div class="flex flex-col justify-between md:flex-row">
-            <a :href="post.url" target="_blank" rel="noopener noreferrer" class="w-full mb-2 text-lg font-medium md:text-xl text-gray-100">{{ post.title }}</a>
-            <p class="w-32 mb-4 text-left text-gray-500 md:text-right md:mb-0">{{ post.by }}</p>
+            <a :href="story.url" target="_blank" rel="noopener noreferrer" class="w-full mb-2 text-lg font-medium md:text-xl text-gray-100">{{ story.title }}</a>
+            <p class="w-32 mb-4 text-left text-gray-500 md:text-right md:mb-0">{{ story.by }}</p>
           </div>
         </div>
       </a>
@@ -31,26 +31,37 @@ export default {
       loading: true,
       stories_ids: [],
       stories: [],
+      start_number: 0,
+      end_number: 20,
     };
   },
 
-  methods: {},
+  methods: {
+    async fetchStories(ids) {
+      ids.forEach((id) => {
+        axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then((response) => {
+          this.stories.push(response.data);
+        });
+      });
+    },
+  },
 
   async created() {
     document.title = "YCombinator Stories | theNews";
     const stories = await axios.get("https://hacker-news.firebaseio.com/v0/topstories.json");
     this.stories_ids = stories.data;
-    this.stories_ids.forEach((id) => {
-      axios
-        .get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-        .then((response) => {
-          this.stories.push(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    });
+    this.fetchStories(this.stories_ids.slice(this.start_number, this.end_number));
+    this.start_number += 20;
+    this.end_number += 20;
     this.loading = false;
+
+    window.onscroll = () => {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        this.fetchStories(this.stories_ids.slice(this.start_number, this.end_number));
+        this.start_number += 20;
+        this.end_number += 20;
+      }
+    };
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,23 @@ module.exports = {
     msTileColor: "#000000",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
+    workboxOptions: {
+      skipWaiting: true,
+    },
+    manifestOptions: {
+      icons: [
+        {
+          src: "/android-chrome-192x192.png",
+          src: "public/img/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "public/img/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
   },
 };
